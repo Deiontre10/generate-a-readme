0cd787688450a1a225e29a59764c74004d7e52fb
Deiontre10 <travontazlowry@gmail.com>
2022-12-20 12:01:39 -0500

    added a README.md template to the generateMarkdown.js

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -12,8 +12,38 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+    ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  Packages required to run this program are: ${data.installation}
+  
+  ## Usage
+  Examples of how to use this program: ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributors
+  ${data.contributer}
+
+  ## Tests
+  To test, run the following command: ${data.tests}
+
+  ## Questions
+  If you have any questions, contact ${data.email}
+  
 `;
 }
 
