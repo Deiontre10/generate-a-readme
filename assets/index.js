3acf5ca6abfcc8ff21104ba8376e356e85552995
Deiontre10 <travontazlowry@gmail.com>
2022-12-20 10:52:58 -0500

    added a base question and more packages

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const prompt = inquirer.createPromptModule();
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to name your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to name your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to name your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to name your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What would you like to name your project?"
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
